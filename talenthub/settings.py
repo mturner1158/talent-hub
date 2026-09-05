d9841d10f57067efe68c2a34f96be47235acc888
mturner1158 <mpt4583@gmail.com>
2026-09-05 16:29:40 +0100

    ready fro deployment to heroku

--- talenthub/settings.py
+++ talenthub/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp','localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['.herokuapp.com','localhost', '127.0.0.1']
 
 
 # Application definition
