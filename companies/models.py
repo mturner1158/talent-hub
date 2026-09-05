ed94ecbcf11e4bf8c309a18a702978a518a58e07
mturner1158 <mpt4583@gmail.com>
2026-09-05 16:49:11 +0100

    set up companies model

--- companies/models.py
+++ companies/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Companies(models.Model):
+    """
+    Contains a profile for each company which has signed up for Talent Hub
+    """
+    company_name = models.CharField(max_length=200, unique=True)
+    description = models.TextField()
+    website = models.CharField()
+    sectors = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __string__(self):
+        return f"{self.company_name} profile"
+
+    class Meta:
+        ordering = ['company_name']
