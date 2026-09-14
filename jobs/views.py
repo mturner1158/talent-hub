4b856acbd6c9f11a8966b8fa12bbc022f34e96ee
mturner1158 <mpt4583@gmail.com>
2026-09-14 21:20:41 +0100

    added static files and base web template

--- jobs/views.py
+++ jobs/views.py
@@ -6,4 +6,5 @@ from .models import Job
 class JobList(generic.ListView):
     queryset = Job.objects.all()
     template_name = "job_list.html"
+    paginate_by = 4
 
--- talenthub/settings.py
+++ talenthub/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com','localhost', '127.0.0.1']
 
@@ -142,6 +142,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Email
